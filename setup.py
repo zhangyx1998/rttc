1bad3f70f103bc8b5f246dc5b1850f2ca61c27ee
Yuxuan Zhang <github@z-yx.cc>
2024-11-06 03:05:47 -0500

    version: 1.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(HOME / "README.md", encoding="utf-8") as f:
 
 setup(
     name="rttc",
-    version="1.0.3",
+    version="1.0.4",
     description="Python run-time type check",
     long_description=long_description,
     long_description_content_type="text/markdown",
