1fba7f5ff14e9ec434d0624e52fa71944c78951e
Yuxuan Zhang <github@z-yx.cc>
2024-11-06 18:12:02 -0500

    update tests/04-unions.py: fixed minor error

--- tests/04-unions.py
+++ tests/04-unions.py
@@ -2,4 +2,4 @@ from . import Test, type_check
 
 # Union types are also supported
 Test(type_check, [[1, 2], ["3", "4"]], list[list[int] | list[str]]) >> True
-Test(type_check, [[1, "2"], [3, "4"]], list[list[int] | list[float]]) >> False
+Test(type_check, [[1, "2"], [3, "4"]], list[list[int] | list[str]]) >> False
