906a1675d13b76e66fe17a384cb046fdb7d93386
Yuxuan Zhang <github@z-yx.cc>
2024-11-06 14:15:07 -0500

    update tests: align with standard, avoid using `list[T1, T2, ...]`

--- tests/02-multiple.py
+++ tests/02-multiple.py
@@ -1,9 +1,9 @@
 from . import Test, type_check
 
 # List
-Test(type_check, [1, "2", 3.0], list[int, str, float]) >> True
-Test(type_check, [1, "2", "3"], list[int, str, float]) >> False
-Test(type_check, [1, "2"], list[int, str, float]) >> False
+Test(type_check, (1, "2", 3.0), tuple[int, str, float]) >> True
+Test(type_check, (1, "2", "3"), tuple[int, str, float]) >> False
+Test(type_check, (1, "2"), tuple[int, str, float]) >> False
 
 # Tuple
 Test(type_check, (1, "2", 3.0), tuple[int, str, float]) >> True
